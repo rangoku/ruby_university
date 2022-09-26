6c30f2b074e5e79bdecf3866c383761d5bddf544
Yevhen Oliinyk <yevhene@realplusonline.com>
2022-09-26 10:12:28 +0300

    Implement calculator.

---
+++ calculator.rb
@@ -0,0 +1,26 @@
+loop do
+  puts "Enter 3 numbers:"
+  numbers = []
+  3.times do
+    numbers.push(gets.to_i)
+  end
+
+  puts "Select operation:"
+
+  begin
+    res = case gets.delete"\n"
+          when "+" then numbers.reduce(:+)
+          when "-" then numbers.reduce(:-)
+          when "*" then numbers.reduce(:*)
+          when "/" then numbers.reduce(:/)
+          else nil
+          end
+  rescue ZeroDivisionError
+    puts "Cannot divide by 0"
+    res = nil
+  end
+
+  if res != nil
+    puts res
+  end
+end
